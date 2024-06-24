b8782dc1b88d8d88503ba545a32b652e220b6b61
briansoon <brian1272k@gmail.com>
2024-06-24 16:11:37 +0800

    Completed

---
+++ 1_GettingStarted/1_GettingStarted.v
@@ -0,0 +1,4 @@
+// Exercise 1-1: Getting Started
+module top_module (output one);
+    assign one = 1'b1;
+endmodule
---
+++ 1_GettingStarted/2_OutputZero.v
@@ -0,0 +1,4 @@
+// Exercise 1-2: Output Zero
+module top_module (output zero);
+    assign zero = 1'b0;
+endmodule
